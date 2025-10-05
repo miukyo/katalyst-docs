1b5905c689cc2d43310f05494daa6928fd54e033
miukyo <zaidaliyhijja@gmail.com>
2025-10-06 00:44:17 +0700

    feat: static search

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,7 +1,15 @@
-'use client';
-import type { ReactNode } from 'react';
-import { RootProvider } from 'fumadocs-ui/provider/waku';
+"use client";
+import type { ReactNode } from "react";
+import { RootProvider } from "fumadocs-ui/provider/waku";
+import SearchDialog from './search';
 
 export function Provider({ children }: { children: ReactNode }) {
-  return <RootProvider>{children}</RootProvider>;
+  return (
+    <RootProvider
+      search={{
+        SearchDialog,
+      }}>
+      {children}
+    </RootProvider>
+  );
 }
