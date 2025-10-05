7f6ac9c0a72f02ad176fa0ea314ec192aec8a09a
miukyo <zaidaliyhijja@gmail.com>
2025-10-06 01:07:20 +0700

    fix: update for static search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,4 +1,9 @@
 import { createFromSource } from 'fumadocs-core/search/server';
 import { source } from '@/lib/source';
 
-export const { GET } = createFromSource(source);
+export const { staticGET: GET } = createFromSource(source);
+
+// statically cached
+export const getConfig = async () => ({
+  render: 'static',
+});
